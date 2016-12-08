bd05ccaf596efa2cd39e7de4fa0692e3f7178217
Alexander Laarmann <alaarmann@gmx.net>
2016-12-08 21:15:02 +0100

    Add resource name input

--- src/Activities.js
+++ src/Activities.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-export default ({onCreateButtonClick})=> (
-  <div className="Activities">
+export default ({onCreateButtonClick})=> {
+  var newResourceNameInput = null;
+
+  return <div className="Activities">
+    <input
+      type="text"
+      ref={(input) => { newResourceNameInput = input; }} />
+
     <button onClick={e => {
          e.preventDefault()
-         onCreateButtonClick("New Resource")
+         onCreateButtonClick(newResourceNameInput.value)
        }}>+</button> 
   </div>
-)
+}
