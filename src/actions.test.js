0885090a85072af83d30b66caebcdea9b934760a
Alexander Laarmann <alaarmann@gmx.net>
2017-01-20 22:49:08 +0100

    Add tests for async action fetchResourcesIfNeeded

--- src/actions.test.js
+++ src/actions.test.js
@@ -25,6 +25,40 @@ describe('Asyncronous actions', () => {
         nock.cleanAll()
     })
 
+    describe('fetchResourcesIfNeeded', () => {
+
+        describe('needsFetch is true', () => {
+            it('should create REQUEST_RESOURCES, execute fetch and create action RECEIVE_RESOURCES', () => {
+                const resourcesPayload = '[{"_id":"587fdfbb9a20f21027a5b96f","key":"My latest Resource","__v":0},{"_id":"587fdfcb9a20f21027a5b970","key":"Another Resource","__v":0}]'
+                nock('http://localhost:8080')
+                    .get('/infracc/resources')
+                    .reply(200, resourcesPayload)
+
+                const initialState = {
+                    resources: {
+                        isFetching: false,
+                        needsRefresh: true,
+                        items: []
+                    }
+                }
+
+                const store = mockStore(initialState)
+
+                return store.dispatch(actions.fetchResourcesIfNeeded())
+                    .then(() => { // return of async actions
+                        // mockStore offers triggered actions via getActions()
+                        expect(store.getActions()[0]).toEqual({type: actions.REQUEST_RESOURCES})
+                        expect(store.getActions()[1].receivedAt).toBeDefined
+                        expect(store.getActions()[1].type).toBe(actions.RECEIVE_RESOURCES)
+                        expect(store.getActions()[1].resources).toEqual(JSON.parse(resourcesPayload))
+
+                    })
+
+
+            })
+        })
+    })
+
     describe('addResource', () => {
         it('should create REQUEST_CREATE_RESOURCE, execute fetch and create action RECEIVE_CREATE_RESOURCE', () => {
             const newResource = {prop1 : 'val1', prop2 : 'val2'}
