c5fe89b11b2a070aae1818ff30b6a21128c05857
Alexander Laarmann <alaarmann@gmx.net>
2017-01-20 05:56:49 +0100

    Add tests for async action createResource

--- src/actions.test.js
+++ src/actions.test.js
@@ -1,5 +1,10 @@
 import * as actions from './actions'
 
+// for async tests
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import nock from 'nock'
+
 describe('filterResources', () => {
     it('should create an action to filter resources', () => {
         const filterExpression = 'anExpression'
@@ -10,4 +15,38 @@ describe('filterResources', () => {
         expect(actions.filterResources(filterExpression)).toEqual(expectedAction)
     })
 })
+
+
+describe('Asyncronous actions', () => {
+    const middlewares = [ thunk ]
+    const mockStore = configureMockStore(middlewares)
+
+    afterEach(() => {
+        nock.cleanAll()
+    })
+
+    describe('addResource', () => {
+        it('should create REQUEST_CREATE_RESOURCE, execute fetch and create action RECEIVE_CREATE_RESOURCE', () => {
+            const newResource = {prop1 : 'val1', prop2 : 'val2'}
+            const payload = 'prop1=val1&prop2=val2';
+            nock('http://localhost:8080')
+                .post('/infracc/resources', payload)
+                .reply(200, {})
+
+            const expectedActions = [
+                { type: actions.REQUEST_CREATE_RESOURCE },
+                { type: actions.RECEIVE_CREATE_RESOURCE }
+            ]
+            const store = mockStore({ creator: {isFetching : false} })
+
+            return store.dispatch(actions.addResource(newResource))
+                .then(() => { // return of async actions
+                    expect(store.getActions()).toEqual(expectedActions)
+                })
+
+
+        })
+    })
+})
+
 
