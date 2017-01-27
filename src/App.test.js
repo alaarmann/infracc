f0298332e7523a3c27d1e06e81654c18acedd6cf
Alexander Laarmann <alaarmann@gmx.net>
2017-01-27 01:35:10 +0100

    Add component test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+it('renders itself', () => {
+  const enzymeWrapper = shallow(<App></App>)
+    const divWrapper = enzymeWrapper.find('div')
+    expect(divWrapper.hasClass('app')).toBe(true)
+//    expect(divWrapper.find('ActionFilter').exists()).toBe(true)
+//    expect(divWrapper.find('ActionActivities').exists()).toBe(true)
+//    expect(divWrapper.find('VisibleResources').exists()).toBe(true)
+//    expect(divWrapper.find('MessagesContainer').exists()).toBe(true)
 });
