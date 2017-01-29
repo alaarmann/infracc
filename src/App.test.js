61b8cbc7753cc8acde7edf793e14d96b0b72746b
Alexander Laarmann <alaarmann@gmx.net>
2017-01-29 11:15:14 +0100

    Add test for content

--- src/App.test.js
+++ src/App.test.js
@@ -6,8 +6,5 @@ it('renders itself', () => {
   const enzymeWrapper = shallow(<App></App>)
     const divWrapper = enzymeWrapper.find('div')
     expect(divWrapper.hasClass('app')).toBe(true)
-//    expect(divWrapper.find('ActionFilter').exists()).toBe(true)
-//    expect(divWrapper.find('ActionActivities').exists()).toBe(true)
-//    expect(divWrapper.find('VisibleResources').exists()).toBe(true)
-//    expect(divWrapper.find('MessagesContainer').exists()).toBe(true)
+    expect(divWrapper.find('Connect(Component)').getNodes().length).toBe(4)
 });
