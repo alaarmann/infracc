c171c54c1c8ea627cafc0c631a8c572bf4b31214
Alexander Laarmann <alaarmann@gmx.net>
2017-02-18 00:37:41 +0100

    Use AlertMessage for LastUpdated

--- src/AlertMessage.js
+++ src/AlertMessage.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Alert } from 'react-bootstrap'
 
-export default ({message, ...props}) => {
+export default ({message, style="danger", ...props}) => {
 
     if (typeof message === 'string') {
-        return <Alert bsStyle="danger">
+        return <Alert bsStyle={style}>
             {message}
         </Alert>
     } else {
--- src/Resources.js
+++ src/Resources.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { ListGroup, ListGroupItem} from 'react-bootstrap'
+import AlertMessage from './AlertMessage'
 
 export default ({resources, isFetching, lastUpdated, ...props}) => (
   <div className="Resources" style={{ opacity: isFetching ? 0.5 : 1 }}>
-      {lastUpdated &&
-      <span>
-              Last updated at {new Date(lastUpdated).toLocaleTimeString()}
-      </span>
-      }
-
+      <AlertMessage style="info" message={lastUpdated && `Last updated at ${new Date(lastUpdated).toLocaleTimeString()}`} />
       <ListGroup>
         {resources.map(resource => (
           <ListGroupItem key={resource._id}>{resource.key}</ListGroupItem>
