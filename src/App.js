217181296488018e28e747ac43f7f146522aa82d
Alexander Laarmann <alaarmann@gmx.net>
2017-02-18 00:41:39 +0100

    Add page header

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import ActivitiesConnector from './ActivitiesConnector';
 import VisibleResources from './VisibleResources';
 import MessagesConnector from './MessagesConnector';
 import ResourceEditorConnector from './ResourceEditorConnector';
+import { PageHeader } from 'react-bootstrap'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+          <PageHeader>Resources</PageHeader>
         <FilterConnector/>
         <ActivitiesConnector/>
 	<VisibleResources/>
