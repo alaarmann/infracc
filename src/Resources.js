ca5c0d33c56e3196f540f3bfb2aea2c8c71cf426
Alexander Laarmann <alaarmann@gmx.net>
2017-02-21 21:18:21 +0100

    Beautify layout

--- src/Resources.js
+++ src/Resources.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ListGroup, ListGroupItem, Button, Glyphicon} from 'react-bootstrap'
+import { ListGroup, ListGroupItem, Button, Glyphicon, Grid, Row, Col, Label} from 'react-bootstrap'
 import AlertMessage from './AlertMessage'
 
 export default ({resources, isFetching, lastUpdated, onDeleteButtonClick, isDeleteButtonDisabled, ...props}) => (
@@ -7,11 +7,22 @@ export default ({resources, isFetching, lastUpdated, onDeleteButtonClick, isDele
       <AlertMessage type="info" message={lastUpdated && `Last updated at ${new Date(lastUpdated).toLocaleTimeString()}`} />
       <ListGroup>
         {resources.map(resource => (
-          <ListGroupItem key={resource._id}>{resource.key}
-              <Button onClick={e => {
-                  e.preventDefault()
-                  onDeleteButtonClick(resource)
-              }} disabled={isDeleteButtonDisabled}><Glyphicon glyph="remove" /></Button>
+          <ListGroupItem key={resource._id}>
+              <Grid>
+                  <Row className="show-grid">
+                      <Col md={7}>
+                          <h4>
+                              <Label bsStyle="primary">{resource.key}</Label>
+                          </h4>
+                      </Col>
+                      <Col>
+                          <Button onClick={e => {
+                          e.preventDefault()
+                          onDeleteButtonClick(resource)
+                      }} disabled={isDeleteButtonDisabled}><Glyphicon glyph="remove" /></Button>
+                      </Col>
+                  </Row>
+              </Grid>
           </ListGroupItem>
         ))}
       </ListGroup>
