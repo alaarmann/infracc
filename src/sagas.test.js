ba550b46fabac062dc83237432200264e31e4422
Alexander Laarmann <alaarmann@gmx.net>
2017-03-23 20:45:20 +0100

    Add test of root saga

--- src/sagas.test.js
+++ src/sagas.test.js
@@ -3,8 +3,12 @@
  */
 import {call, put} from 'redux-saga/effects'
 import fetch from 'isomorphic-fetch'
-import { RETRIEVE_RESOURCES } from './actions'
+import createSagaMiddleware from 'redux-saga'
+import configureMockStore from 'redux-mock-store'
+import nock from 'nock'
+import { retrieveResourcesSimple, RETRIEVE_RESOURCES, RETRIEVE_RESOURCES_SIMPLE } from './actions'
 import { retrieveResources } from './sagas'
+import saga from './sagas'
 
 describe('retrieveResources', () => {
 
@@ -24,4 +28,40 @@ describe('retrieveResources', () => {
         expect(putAction.PUT.action).toBeDefined()
         expect(putAction.PUT.action.type).toEqual(RETRIEVE_RESOURCES)
     })
+})
+
+describe('saga', () => {
+
+    it('retrieves resources', () => {
+        const resourcesPayload = '[{"_id":"587fdfbb9a20f21027a5b96f","key":"My latest Resource","__v":0},{"_id":"587fdfcb9a20f21027a5b970","key":"Another Resource","__v":0}]'
+        nock('http://localhost:8080')
+            .get('/infracc/resources')
+            .reply(200, resourcesPayload)
+
+        const sagaMiddleware = createSagaMiddleware()
+        const middlewares = [ sagaMiddleware ]
+        const mockStore = configureMockStore(middlewares)
+
+        const store = mockStore({})
+        sagaMiddleware.run(saga)
+
+        store.dispatch(retrieveResourcesSimple())
+
+        const expectedActionCount = 2
+
+        return new Promise((resolve) =>
+            store.subscribe(
+                () => store.getActions().length === expectedActionCount && resolve()
+            )
+        ).then(() => {
+
+            expect(store.getActions().length).toEqual(expectedActionCount)
+            expect(store.getActions()[0].type).toEqual(RETRIEVE_RESOURCES_SIMPLE)
+            expect(store.getActions()[1].type).toEqual(RETRIEVE_RESOURCES)
+            expect(store.getActions()[1].payload.resources).toEqual(JSON.parse(resourcesPayload))
+        })
+
+    })
+
+
 })
